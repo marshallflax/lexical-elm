8c5e37692c422a4fe415867d553638cbf5dfe63b
marshall flax <marshall.flax@gmail.com>
2016-12-14 09:15:53 -0500

    OnBooleanEvent

--- src/ViewUtil.elm
+++ src/ViewUtil.elm
@@ -1,30 +1,25 @@
 module ViewUtil exposing (onShiftedMouseEnter)
 
 import Json.Decode
-import Html.Events exposing (on)
-import Html exposing (Attribute)
+import Html.Events
+import Html
 
 
-hasModifier : msg -> Bool -> Json.Decode.Decoder msg
-hasModifier message bool =
+onBooleanEvent : String -> String -> (Bool -> Json.Decode.Decoder msg) -> Html.Attribute msg
+onBooleanEvent modifier eventName toMsg =
+    Json.Decode.at [ modifier ] Json.Decode.bool
+        |> Json.Decode.andThen toMsg
+        |> Html.Events.on eventName
+
+
+messageIfTrue : msg -> Bool -> Json.Decode.Decoder msg
+messageIfTrue message bool =
     if bool then
         Json.Decode.succeed message
     else
         Json.Decode.fail ("No match")
 
 
-onModifiedEvent : String -> String -> msg -> Attribute msg
-onModifiedEvent modifier eventName message =
-    let
-        toMsg : Bool -> Json.Decode.Decoder msg
-        toMsg =
-            hasModifier message
-    in
-        Json.Decode.at [ modifier ] Json.Decode.bool
-            |> Json.Decode.andThen toMsg
-            |> Html.Events.on eventName
-
-
-onShiftedMouseEnter : msg -> Attribute msg
+onShiftedMouseEnter : msg -> Html.Attribute msg
 onShiftedMouseEnter message =
-    onModifiedEvent "shiftKey" "mouseenter" message
+    onBooleanEvent "shiftKey" "mouseenter" (messageIfTrue message)
