9c587bdc8cc1a936f2a1db86828aa2d3d6d1ca3c
marshall flax <marshall.flax@gmail.com>
2016-12-14 09:07:57 -0500

    prepare to make onModifiedEvent more generic

--- src/ViewUtil.elm
+++ src/ViewUtil.elm
@@ -5,21 +5,26 @@ import Html.Events exposing (on)
 import Html exposing (Attribute)
 
 
+hasModifier : msg -> Bool -> Json.Decode.Decoder msg
+hasModifier message bool =
+    if bool then
+        Json.Decode.succeed message
+    else
+        Json.Decode.fail ("No match")
+
+
 onModifiedEvent : String -> String -> msg -> Attribute msg
 onModifiedEvent modifier eventName message =
     let
-        hasModifier : Bool -> Json.Decode.Decoder msg
-        hasModifier bool =
-            if bool then
-                Json.Decode.succeed message
-            else
-                Json.Decode.fail ("No " ++ modifier)
+        toMsg : Bool -> Json.Decode.Decoder msg
+        toMsg =
+            hasModifier message
     in
         Json.Decode.at [ modifier ] Json.Decode.bool
-            |> Json.Decode.andThen hasModifier
+            |> Json.Decode.andThen toMsg
             |> Html.Events.on eventName
 
 
 onShiftedMouseEnter : msg -> Attribute msg
-onShiftedMouseEnter =
-    onModifiedEvent "shiftKey" "mouseenter"
+onShiftedMouseEnter message =
+    onModifiedEvent "shiftKey" "mouseenter" message
