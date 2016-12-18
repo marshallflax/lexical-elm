552d93cc328a739f27d42d52b6d60e7097e513e7
marshall flax <marshall.flax@gmail.com>
2016-12-18 09:23:56 -0500

    Passing first round of frameification tdd

--- src/BowlingScore.elm
+++ src/BowlingScore.elm
@@ -1,5 +1,6 @@
 module BowlingScore exposing (testResults)
 
+import Array exposing (..)
 import List exposing (foldl)
 import Testing exposing (TestResult)
 
@@ -66,17 +67,43 @@ testFrameIfication =
      in
         List.map testGame
             [ { throws = [ 1 ], expected = [ Partial 1 ] }
-            , { throws = [ 1, 1 ], expected = [ Open 2 ] }
-            , { throws = [ 1, 1, 1 ], expected = [ Open 2, Partial 1 ] }
+            , { throws = [ 1, 1 ], expected = [ Open 1 1 ] }
+            , { throws = [ 1, 1, 1 ], expected = [ Open 1 1, Partial 1 ] }
             ]
     )
 
 
-frameify : Throws -> List Frame
-frameify throws =
-    []
-
-
 type Frame
     = Partial Int
-    | Open Int
+    | Open Int Int
+
+
+frameify : Throws -> List Frame
+frameify throws =
+    let
+        ( remainingThrows, computedFrame ) =
+            frameHelper throws Array.empty
+    in
+        Array.toList computedFrame
+
+
+frameHelper : Throws -> Array Frame -> ( Throws, Array Frame )
+frameHelper throws currentFrames =
+    let
+        head =
+            List.head throws
+
+        next =
+            List.head (List.drop 1 throws)
+    in
+        case head of
+            Nothing ->
+                ( throws, currentFrames )
+
+            Just throw1 ->
+                case next of
+                    Nothing ->
+                        frameHelper (List.drop 2 throws) (Array.push (Partial throw1) currentFrames)
+
+                    Just throw2 ->
+                        frameHelper (List.drop 2 throws) (Array.push (Open throw1 throw2) currentFrames)
