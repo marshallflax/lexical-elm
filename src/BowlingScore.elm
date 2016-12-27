1a09c9810a6201127b5e46ca4f1529acc4a92eb5
marshall flax <marshall.flax@gmail.com>
2016-12-27 18:02:45 -0500

    Explaining partitionBy

--- src/BowlingScore.elm
+++ src/BowlingScore.elm
@@ -190,25 +190,34 @@ score throws =
 
 partitionBy : (List a -> Bool) -> Transducer a (List a) r (List a)
 partitionBy pred =
-    { init =
-        \reduce r -> ( [], r )
-    , step =
-        \reduce input ( hold, r ) ->
-            let
-                merged =
-                    input :: hold
-            in
-                if (pred merged) then
-                    ( [], reduce (List.reverse merged) r )
+    let
+        doReduce : (List a -> b -> c) -> List a -> b -> c
+        doReduce reduce list currentReduction =
+            reduce (List.reverse list) currentReduction
+
+        doAppend : a -> List a -> List a
+        doAppend input state =
+            input :: state
+    in
+        { init =
+            \reduce r -> ( [], r )
+        , step =
+            \reduce input ( state, currentReduction ) ->
+                let
+                    merged =
+                        doAppend input state
+                in
+                    if (pred merged) then
+                        ( [], doReduce reduce merged currentReduction )
+                    else
+                        ( merged, currentReduction )
+        , complete =
+            \reduce ( state, currentReduction ) ->
+                if (List.isEmpty state) then
+                    currentReduction
                 else
-                    ( merged, r )
-    , complete =
-        \reduce ( hold, r ) ->
-            if (List.isEmpty hold) then
-                r
-            else
-                reduce (List.reverse hold) r
-    }
+                    doReduce reduce state currentReduction
+        }
 
 
 frameify : Throws -> List Frame
