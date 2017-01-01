1823764a0f1477711b8c009d0b4a123c1c267788
marshall flax <marshall.flax@gmail.com>
2017-01-01 16:26:02 -0500

    Split huge view function

--- src/LexicalView.elm
+++ src/LexicalView.elm
@@ -22,14 +22,107 @@ showSaveButton : Html Msg
 showSaveButton =
     button [ onClick SaveModel ] [ text "Save" ]
 
-showTextInput : LexicalModel ->  Html Msg
+
+showTextInput : LexicalModel -> Html Msg
 showTextInput lexicalModel =
     input
-            [ value (dumpState lexicalModel)
-            , onInput (\text -> LexicalMessage (SetText text))
-            , style [ ( "width", "800px" ) ]
+        [ value (dumpState lexicalModel)
+        , onInput (\text -> LexicalMessage (SetText text))
+        , style [ ( "width", "800px" ) ]
+        ]
+        []
+
+
+colorButtons : LexicalModel -> Html Msg
+colorButtons lexicalModel =
+    div
+        []
+        (let
+            enableButton : List String -> Html Msg
+            enableButton cs =
+                button [ onClick (LexicalMessage (HideSomeColors cs)) ] [ text "hide" ]
+
+            disableButton : List String -> Html Msg
+            disableButton cs =
+                button [ onClick (LexicalMessage (ResetSomeColors cs)) ] [ text "reset" ]
+
+            doCell : String -> Html Msg
+            doCell l =
+                td []
+                    [ input
+                        [ type_ "checkbox"
+                        , onClick (LexicalMessage (ToggleColorEnabled l))
+                        , checked (Set.member l lexicalModel.hideColors)
+                        ]
+                        []
+                    , button [ Html.Attributes.attribute "id" ("colorButton" ++ l), colorStyle l, onClick (LexicalMessage (SetCurrentColor l)) ] [ text l ]
+                    ]
+
+            doRow : List String -> Html Msg
+            doRow ls =
+                table [] [ tr [] (enableButton ls :: disableButton ls :: (List.map doCell ls)) ]
+         in
+            List.map doRow rainbowList
+        )
+
+
+resetButtons : Html Msg
+resetButtons =
+    button [ onClick (LexicalMessage EnableAllColors) ] [ text "ResetHiding" ]
+
+
+wordsPerLine : LexicalModel -> Html Msg
+wordsPerLine lexicalModel =
+    input
+        [ value (toString lexicalModel.wordsPerLine)
+        , onInput (\number -> LexicalMessage (SetWordsPerLine number))
+        ]
+        [ text "WordsPerLine" ]
+
+
+wordStats : LexicalModel -> Html Msg
+wordStats lexicalModel =
+    span []
+        [ text (toString (countWordsMatching lexicalModel) ++ "/" ++ (toString (countWords lexicalModel))) ]
+
+
+wordsForColor : LexicalModel -> Html Msg
+wordsForColor lexicalModel =
+    input
+        [ value (String.join ", " (matchingWordsForColor lexicalModel.workingColor lexicalModel.words |> Array.toList))
+        , style [ ( "width", "800px" ) ]
+        , readonly True
+        , colorStyle lexicalModel.workingColor
+        ]
+        []
+
+
+frequencyStats : LexicalModel -> Html Msg
+frequencyStats lexicalModel =
+    Html.table
+        []
+        [ tr []
+            [ td [ style [ ( "width", "800px" ), ( "vertical-align", "top" ) ] ]
+                (let
+                    doWord : ( Int, ColoredWord ) -> Html Msg
+                    doWord =
+                        renderWord lexicalModel.hideColors lexicalModel.workingColor lexicalModel.workingNormalized
+
+                    renderLine : List ( Int, ColoredWord ) -> Html Msg
+                    renderLine listPart =
+                        Html.div [ stylesheet.class MyClass ]
+                            (List.map doWord listPart)
+                 in
+                    List.map renderLine (partitionedList lexicalModel)
+                )
+            , td [ style [ ( "width", "800px" ), ( "vertical-align", "top" ) ] ]
+                [ FreqInfoView.renderFrequencies lexicalModel.workingNormalized lexicalModel.frequencies.words ]
+            , td [ style [ ( "width", "400px" ), ( "vertical-align", "top" ) ] ]
+                [ FreqInfoView.renderFrequencies lexicalModel.workingNormalized lexicalModel.frequencies.n2 ]
             ]
-            []
+        ]
+
+
 viewLexicalModel : LexicalModel -> Html Msg
 viewLexicalModel lexicalModel =
     div []
@@ -37,83 +130,14 @@ viewLexicalModel lexicalModel =
         , p [] []
         , showSaveButton
         , showTextInput lexicalModel
-        , div
-            []
-            (let
-                doCell : String -> Html Msg
-                doCell l =
-                    td []
-                        [ input
-                            [ type_ "checkbox"
-                            , onClick (LexicalMessage (ToggleColorEnabled l))
-                            , checked (Set.member l lexicalModel.hideColors)
-                            ]
-                            []
-                        , button [ Html.Attributes.attribute "id" ("colorButton" ++ l), colorStyle l, onClick (LexicalMessage (SetCurrentColor l)) ] [ text l ]
-                        ]
-
-                doRow : List String -> Html Msg
-                doRow ls =
-                    table [] [ tr [] (enableButton ls :: disableButton ls :: (List.map doCell ls)) ]
-             in
-                List.map doRow rainbowList
-            )
-        , Html.p
-            []
-            [ button [ onClick (LexicalMessage EnableAllColors) ] [ text "ResetHiding" ]
-            , input
-                [ value (toString lexicalModel.wordsPerLine)
-                , onInput (\number -> LexicalMessage (SetWordsPerLine number))
-                ]
-                [ text "WordsPerLine" ]
-            , span []
-                [ text
-                    (toString (countWordsMatching lexicalModel)
-                        ++ "/"
-                        ++ (toString (countWords lexicalModel))
-                    )
-                ]
-            ]
-        , input
-            [ value (String.join ", " (matchingWordsForColor lexicalModel.workingColor lexicalModel.words |> Array.toList))
-            , style [ ( "width", "800px" ) ]
-            , readonly True
-            , colorStyle lexicalModel.workingColor
-            ]
-            []
-        , Html.table
-            []
-            [ tr []
-                [ td [ style [ ( "width", "800px" ), ( "vertical-align", "top" ) ] ]
-                    (let
-                        doWord : ( Int, ColoredWord ) -> Html Msg
-                        doWord =
-                            renderWord lexicalModel.hideColors lexicalModel.workingColor lexicalModel.workingNormalized
-
-                        renderLine : List ( Int, ColoredWord ) -> Html Msg
-                        renderLine listPart =
-                            Html.div [ stylesheet.class MyClass ]
-                                (List.map doWord listPart)
-                     in
-                        List.map renderLine (partitionedList lexicalModel)
-                    )
-                , td [ style [ ( "width", "800px" ), ( "vertical-align", "top" ) ] ]
-                    [ FreqInfoView.renderFrequencies lexicalModel.workingNormalized lexicalModel.frequencies.words ]
-                , td [ style [ ( "width", "400px" ), ( "vertical-align", "top" ) ] ]
-                    [ FreqInfoView.renderFrequencies lexicalModel.workingNormalized lexicalModel.frequencies.n2 ]
-                ]
-            ]
+        , colorButtons lexicalModel
+        , p [] []
+        , resetButtons
+        , wordsPerLine lexicalModel
+        , wordStats lexicalModel
+        , wordsForColor lexicalModel
+        , frequencyStats lexicalModel
         ]
-
-
-enableButton : List String -> Html Msg
-enableButton cs =
-    button [ onClick (LexicalMessage (HideSomeColors cs)) ] [ text "hide" ]
-
-
-disableButton : List String -> Html Msg
-disableButton cs =
-    button [ onClick (LexicalMessage (ResetSomeColors cs)) ] [ text "reset" ]
 
 
 stylesheet : Css.Stylesheet Id Class msg
