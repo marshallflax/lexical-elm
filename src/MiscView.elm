d05ed8b9272ab9437749ad678b36565f020f26fd
marshall flax <marshall.flax@gmail.com>
2017-01-02 16:17:18 -0500

    more generic

--- src/MiscView.elm
+++ src/MiscView.elm
@@ -5,28 +5,29 @@ import Html.Events
 import Json.Decode as Decode
 
 
-forBooleanModifier : String -> String -> (Bool -> Bool) -> msg -> Html.Attribute msg
-forBooleanModifier modifier eventName modifierCriteria message =
+forBooleanModifier : Decode.Decoder a -> String -> (a -> Bool) -> msg -> Html.Attribute msg
+forBooleanModifier decoder eventName modifierCriteria message =
     let
-        extractModfier : Decode.Decoder Bool
-        extractModfier =
-            Decode.at [ modifier ] Decode.bool
-
-        matches : Bool -> Decode.Decoder msg
+        matches : a -> Decode.Decoder msg
         matches modifierValue =
             if (modifierCriteria modifierValue) then
                 Decode.succeed message
             else
                 Decode.fail ("Not relevant")
     in
-        extractModfier |> Decode.andThen matches |> Html.Events.on eventName
+        decoder |> Decode.andThen matches |> Html.Events.on eventName
+
+
+decodeShiftKey : Decode.Decoder Bool
+decodeShiftKey =
+    Decode.at [ "shiftKey" ] Decode.bool
 
 
 onShiftedMouseEnter : msg -> Html.Attribute msg
 onShiftedMouseEnter =
-    forBooleanModifier "shiftKey" "mouseenter" identity
+    forBooleanModifier decodeShiftKey "mouseenter" identity
 
 
 onUnShiftedMouseEnter : msg -> Html.Attribute msg
 onUnShiftedMouseEnter =
-    forBooleanModifier "shiftKey" "mouseenter" not
+    forBooleanModifier decodeShiftKey "mouseenter" not
