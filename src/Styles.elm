f701edbf3aaabd774623483b877e6810a950166d
marshall flax <marshall.flax@gmail.com>
2017-01-28 11:10:43 -0500

    Add more styles (not yet in use)

--- src/Styles.elm
+++ src/Styles.elm
@@ -27,6 +27,8 @@ type Class
     | TestBad
     | DisplayNone
     | Colophon
+    | SolidBlackBorder
+    | SolidTransparentBorder
 
 
 imports : List String
@@ -36,31 +38,15 @@ imports =
 
 rules : List { descriptor : Css.Descriptor, selectors : List (Css.Sel Id Class) }
 rules =
-    [ { selectors = [ Css.Class NumberLineClass ]
-      , descriptor = [ ( "counter-increment", "line" ) ]
-      }
-    , { selectors = [ Css.Pseudo [ Css.Before ] (Css.Class NumberLineClass) ]
-      , descriptor = [ ( "content", "counter(line)" ), ( "color", "red" ) ]
-      }
-    , { selectors = [ Css.Class GradientClass ]
-      , descriptor = [ ( "background-clip", "content-box !important" ) ]
-      }
-    , { selectors = [ Css.Class Cell400px ]
-      , descriptor = [ ( "width", "400px" ), ( "vertical-align", "top" ) ]
-      }
-    , { selectors = [ Css.Class Cell800px ]
-      , descriptor = [ ( "width", "800px" ), ( "vertical-align", "top" ) ]
-      }
-    , { selectors = [ Css.Class TestGood ]
-      , descriptor = [ ( "background", "lightgreen" ) ]
-      }
-    , { selectors = [ Css.Class TestBad ]
-      , descriptor = [ ( "background", "red" ) ]
-      }
-    , { selectors = [ Css.Class DisplayNone ]
-      , descriptor = [ ( "display", "none" ) ]
-      }
-    , { selectors = [ Css.Class Colophon ]
-      , descriptor = [ ( "font-size", "25%" ) ]
-      }
+    [ { selectors = [ Css.Class NumberLineClass ], descriptor = [ ( "counter-increment", "line" ) ] }
+    , { selectors = [ Css.Pseudo [ Css.Before ] (Css.Class NumberLineClass) ], descriptor = [ ( "content", "counter(line)" ), ( "color", "red" ) ] }
+    , { selectors = [ Css.Class GradientClass ], descriptor = [ ( "background-clip", "content-box !important" ) ] }
+    , { selectors = [ Css.Class Cell400px ], descriptor = [ ( "width", "400px" ), ( "vertical-align", "top" ) ] }
+    , { selectors = [ Css.Class Cell800px ], descriptor = [ ( "width", "800px" ), ( "vertical-align", "top" ) ] }
+    , { selectors = [ Css.Class TestGood ], descriptor = [ ( "background", "lightgreen" ) ] }
+    , { selectors = [ Css.Class TestBad ], descriptor = [ ( "background", "red" ) ] }
+    , { selectors = [ Css.Class DisplayNone ], descriptor = [ ( "display", "none" ) ] }
+    , { selectors = [ Css.Class Colophon ], descriptor = [ ( "font-size", "25%" ) ] }
+    , { selectors = [ Css.Class SolidBlackBorder ], descriptor = [ ( "borderStyle", "solid" ), ( "borderColor", "black" ) ] }
+    , { selectors = [ Css.Class SolidTransparentBorder ], descriptor = [ ( "borderStyle", "solid" ), ( "borderColor", "transparent" ) ] }
     ]
