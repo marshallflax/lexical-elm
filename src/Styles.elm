b027479ef44337ff1d824261677a0c88e881116c
marshall flax <marshall.flax@gmail.com>
2017-01-28 11:55:31 -0500

    Minor cleanup

--- src/Styles.elm
+++ src/Styles.elm
@@ -6,7 +6,20 @@ import Html
 
 stylesheet : Css.Stylesheet Id Class msg
 stylesheet =
-    Css.stylesheet imports rules
+    Css.stylesheet []
+        [ { selectors = [ Css.Class NumberLineClass ], descriptor = [ ( "counter-increment", "line" ) ] }
+        , { selectors = [ Css.Pseudo [ Css.Before ] (Css.Class NumberLineClass) ], descriptor = [ ( "content", "counter(line)" ), ( "color", "red" ) ] }
+        , { selectors = [ Css.Class GradientClass ], descriptor = [ ( "background-clip", "content-box !important" ) ] }
+        , { selectors = [ Css.Class Cell400px ], descriptor = [ ( "width", "400px" ), ( "vertical-align", "top" ) ] }
+        , { selectors = [ Css.Class Cell800px ], descriptor = [ ( "width", "800px" ), ( "vertical-align", "top" ) ] }
+        , { selectors = [ Css.Class TestGood ], descriptor = [ ( "background", "lightgreen" ) ] }
+        , { selectors = [ Css.Class TestBad ], descriptor = [ ( "background", "red" ) ] }
+        , { selectors = [ Css.Class DisplayNone ], descriptor = [ ( "display", "none" ) ] }
+        , { selectors = [ Css.Class Colophon ], descriptor = [ ( "font-size", "25%" ) ] }
+        , { selectors = [ Css.Class SolidBlackBorder ], descriptor = [ ( "borderStyle", "solid" ), ( "borderColor", "black" ) ] }
+        , { selectors = [ Css.Class SolidTransparentBorder ], descriptor = [ ( "borderStyle", "solid" ), ( "borderColor", "transparent" ) ] }
+        , { selectors = [ Css.Class OutlineBorder ], descriptor = [ ( "border", "solid" ), ( "border-width", "1px" ) ] }
+        ]
 
 
 useClass : Class -> Html.Attribute msg
@@ -30,25 +43,3 @@ type Class
     | SolidBlackBorder
     | SolidTransparentBorder
     | OutlineBorder
-
-
-imports : List String
-imports =
-    []
-
-
-rules : List { descriptor : Css.Descriptor, selectors : List (Css.Sel Id Class) }
-rules =
-    [ { selectors = [ Css.Class NumberLineClass ], descriptor = [ ( "counter-increment", "line" ) ] }
-    , { selectors = [ Css.Pseudo [ Css.Before ] (Css.Class NumberLineClass) ], descriptor = [ ( "content", "counter(line)" ), ( "color", "red" ) ] }
-    , { selectors = [ Css.Class GradientClass ], descriptor = [ ( "background-clip", "content-box !important" ) ] }
-    , { selectors = [ Css.Class Cell400px ], descriptor = [ ( "width", "400px" ), ( "vertical-align", "top" ) ] }
-    , { selectors = [ Css.Class Cell800px ], descriptor = [ ( "width", "800px" ), ( "vertical-align", "top" ) ] }
-    , { selectors = [ Css.Class TestGood ], descriptor = [ ( "background", "lightgreen" ) ] }
-    , { selectors = [ Css.Class TestBad ], descriptor = [ ( "background", "red" ) ] }
-    , { selectors = [ Css.Class DisplayNone ], descriptor = [ ( "display", "none" ) ] }
-    , { selectors = [ Css.Class Colophon ], descriptor = [ ( "font-size", "25%" ) ] }
-    , { selectors = [ Css.Class SolidBlackBorder ], descriptor = [ ( "borderStyle", "solid" ), ( "borderColor", "black" ) ] }
-    , { selectors = [ Css.Class SolidTransparentBorder ], descriptor = [ ( "borderStyle", "solid" ), ( "borderColor", "transparent" ) ] }
-    , { selectors = [ Css.Class OutlineBorder ], descriptor = [ ( "border", "solid" ), ( "border-width", "1px" ) ] }
-    ]
